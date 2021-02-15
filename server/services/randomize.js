a245d00bdb172a240b7f493484eaf7736cc9de14
Alex Votry <youremail@gmail.com>
2021-02-15 11:23:33 -0800

    made appoint teams from 4 or more

--- server/services/randomize.js
+++ server/services/randomize.js
@@ -2,7 +2,7 @@ const getLetter = (letters) => {
   const index = getRandom(letters.length);
   const currentLetter = letters[index];
   letters.splice(index, 1)
-  // return the currentLetter and remove is from the array.
+  // return the currentLetter and remove it from the array.
   return [currentLetter, letters];
 };
 
@@ -22,14 +22,15 @@ function getRandom(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function appointTeams (players) {
+// create teams based on amount of players. no teams for 3 or less.
+function appointTeams(players) {
   const len = players.length;
   const teams = {};
   const teamNames = ["Blue", "Red", "Purple", "Green", "Gold"];
-  
+
   let noOfTeams = 0;
 
-  if (len < 6) {
+  if (len <= 3) {
     noOfTeams = len;
   } else if (len > 9) {
     noOfTeams = 5;
